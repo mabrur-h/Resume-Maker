f648ee6b6dd31081667e03c9cf2bdc028b6eb567
Mabrur <mabrur.h11@gmail.com>
2021-03-11 17:35:16 +0500

    Update AppForm.vue

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -1,8 +1,8 @@
 <template>
-  <form className="card card-w30">
+  <form className="card card-w30" @submit.prevent="submitForm">
     <div className="form-control">
       <label htmlFor="type">Тип блока</label>
-      <select id="type">
+      <select id="type" v-model="textType">
         <option value="title">Заголовок</option>
         <option value="subtitle">Подзаголовок</option>
         <option value="avatar">Аватар</option>
@@ -12,7 +12,7 @@
 
     <div className="form-control">
       <label htmlFor="value">Значение</label>
-      <textarea id="value" rows="3"></textarea>
+      <textarea id="value" rows="3" v-model="textArea"></textarea>
     </div>
 
     <button className="btn primary">Добавить</button>
@@ -21,7 +21,19 @@
 
 <script>
 export default {
-name: "AppForm"
+name: "AppForm",
+  data() {
+  return {
+    textType: '',
+    textArea: ''
+  }
+  },
+  methods: {
+  submitForm() {
+    console.log(this.textType)
+    console.log(this.textArea)
+  }
+  }
 }
 </script>
 
